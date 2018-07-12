39869059ade479ee802d9b4152168e53a223c311
Antonio Coppola <coppola.ant@gmail.com>
2018-07-12 23:40:32 +0100

    start and some styling

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,45 @@
 <template>
-  <div id="app">
-    <div id="nav">
+  <div id="app" class="app">
+    <!-- <div id="nav">
       <router-link to="/">Home</router-link> |
       <router-link to="/about">About</router-link>
-    </div>
-    <router-view/>
+    </div> -->
+    <main class="main-container">
+      <router-view/>
+    </main>
+    <footer class="footer">
+      <p class="footer-text">
+        Made with <i class="icon ion-heart"></i> with <a href="https://cli.vuejs.org/">VueCLI</a>
+      </p>
+    </footer>
   </div>
 </template>
 
-<style lang="scss">
-#app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
+<style lang="scss" scoped>
+.app {
+  position: relative;
+  width: 100%;
+  height: 100%;
 }
-#nav {
-  padding: 30px;
-  a {
-    font-weight: bold;
-    color: #2c3e50;
-    &.router-link-exact-active {
-      color: #42b983;
+.main-container {
+  position: relative;
+  padding: 40px;
+  height: 100%;
+
+  display: flex;
+}
+.footer {
+  position: absolute;
+  bottom: 0;
+  width: 100%;
+  padding: 5px 0;
+  text-align: center;
+
+  &-text {
+    .icon {
+      color: red;
+      margin: 0 2px;
+      font-size: 15px;
     }
   }
 }
