a59a51f87780ad405dd71a47123290b58359cde1
Antonio Coppola <antonio@evemattress.co.uk>
2018-07-21 12:28:47 +0100

    first ready version, gtm, meta tags, content and social links

--- src/App.vue
+++ src/App.vue
@@ -4,49 +4,24 @@
       <router-link to="/">Home</router-link> |
       <router-link to="/about">About</router-link>
     </div> -->
-    <main class="main-container">
+    <main class="main-container theme-dark">
       <router-view/>
     </main>
-    <footer class="footer theme-light">
-      <p class="footer-text">
-        Made with <i class="icon ion-heart"></i> with <a href="https://cli.vuejs.org/">VueCLI</a> in 2018
-      </p>
-      <p class="footer-text">
-        WIP - v{{ $store.state.version }}
-      </p>
-    </footer>
+    <Footer/>
   </div>
 </template>
 
-<style lang="scss" scoped>
-.app {
-  position: relative;
-  width: 100%;
-  height: 100%;
-}
-.main-container {
-  position: relative;
-  height: 100%;
+<script>
+import Footer from '@/components/Footer.vue'
 
-  display: flex;
-}
-.footer {
-  position: absolute;
-  bottom: 0;
-  display: flex;
-  justify-content: space-between;
-
-  width: 100%;
-  padding: 5px 40px;
-  text-align: center;
-
-  &-text {
-    font-size: 12px;
-
-    .icon {
-      color: red;
-      font-size: 12px;
-    }
+export default {
+  name: 'App',
+  components: {
+    Footer
   }
 }
+</script>
+
+<style lang="scss" scoped>
+
 </style>
