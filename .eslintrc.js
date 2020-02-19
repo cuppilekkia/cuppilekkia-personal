9a42e467cf30be2a067c9bed72f5cc0b30acfe3d
Antonio Coppola <vitantonio.coppola@drivek.com>
2020-02-19 18:19:44 +0100

    setting up nuxt

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,27 +1,23 @@
 module.exports = {
   root: true,
   env: {
+    browser: true,
     node: true
-  },
-  'extends': [
-    'plugin:vue/essential',
-    '@vue/standard'
-  ],
-  rules: {
-    'no-console': 'off',
-    'no-debugger': 'off',
-    'vue/html-indent': [
-      'error',
-      4
-    ],
-    'vue/require-default-prop': 'error',
-    'vue/require-prop-types': 'error',
-    'vue/html-quotes': [
-      'error',
-      'double'
-    ]
   },
   parserOptions: {
     parser: 'babel-eslint'
+  },
+  extends: [
+    '@nuxtjs',
+    'prettier',
+    'prettier/vue',
+    'plugin:prettier/recommended',
+    'plugin:nuxt/recommended'
+  ],
+  plugins: [
+    'prettier'
+  ],
+  // add your custom rules here
+  rules: {
   }
 }
